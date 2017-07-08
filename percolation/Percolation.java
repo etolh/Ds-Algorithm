4c9a2d320a4c209353ef657495f2e39a23f2e0a5
etolh <huliang12@126.com>
2017-07-08 19:58:39 +0800

    Percolation: v1 first commit

---
+++ percolation/Percolation.java
@@ -0,0 +1,85 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {    
+    private boolean[][] fopen;
+    private int opencount, up;
+    private static WeightedQuickUnionUF uf;
+    public Percolation(int n) {       
+        if(n<=0)
+            throw new IllegalArgumentException();        
+        int count = n * n;
+        opencount = 0;
+        fopen = new boolean[n][n];
+        uf = new WeightedQuickUnionUF(count);
+        up = 0;
+        for(int i = 0; i < n; i++){
+            uf.union(up, i);
+        }        
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+            fopen[i][j] = false;
+    }
+
+    public void open(int row, int col) {
+        int n = fopen[0].length;
+        if(row < 1  || row > n || col < 1 || col > n)
+            throw new IllegalArgumentException();
+        
+        if(isOpen(row, col))
+            return;
+        
+        opencount++;
+        fopen[row-1][col-1] = true;
+    }
+    public boolean isOpen(int row, int col) {
+        int n = fopen[0].length;
+        if(row < 1  || row > n || col < 1 || col > n)
+            throw new IllegalArgumentException();
+        return fopen[row-1][col-1];
+    }
+    public boolean isFull(int row, int col) {
+        int n = fopen[0].length;
+        if(row < 1  || row > n || col < 1 || col > n)
+            throw new IllegalArgumentException();       
+        if(!isOpen(row, col))
+            return false;
+        int n = fopen[0].length;
+        int nowc = (row-1) * n + col - 1;
+        //上
+        if(row > 1){
+            int upc = nowc - n;
+            if(isOpen(row-1, col)){
+                uf.union(nowc, upc);
+            }
+        }
+        if(row < n){
+            int downc = nowc + n;
+            if(isOpen(row+1, col)){
+                uf.union(nowc, downc);
+            }
+        }        
+        if(col > 1){
+            int leftc = nowc - 1;
+            if(isOpen(row, col-1)){
+                uf.union(nowc, leftc);
+            }
+        }       
+        if(col < n){
+            int rightc = nowc + 1;
+            if(isOpen(row, col+1)){
+                uf.union(nowc, rightc);
+            }
+        }
+        return uf.connected(nowc, up);
+    }
+    public int numberOfOpenSites() {
+        return opencount;
+    }
+    public boolean percolates() {
+        int n = fopen[0].length;       
+        for(int i = n * n - 1; i >= n*n - n; i--)
+            if(uf.connected(i, up))
+                return true;     
+        return false;
+    }   
+}
---
+++ percolation/PercolationStats.java
@@ -0,0 +1,68 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.StdOut;
+
+public class PercolationStats {
+  
+    private static double[] x;
+    private static int n;
+    private static int trials;
+  
+    public PercolationStats(int n, int trials){
+        
+        if(n <= 0 || trials <= 0)
+            throw new IllegalArgumentException();
+        x = new double[trials];
+        
+        for(int i = 0; i < trials; i++){
+            
+            Percolation p = new Percolation(n);
+            boolean flag = false;
+            
+            while(true){
+                int rrow = StdRandom.uniform(1,n+1);
+                int rcol = StdRandom.uniform(1,n+1);          
+                
+                if(!p.isOpen(rrow, rcol))
+                    p.open(rrow, rcol);
+                
+                if(p.isFull(rrow, rcol)){
+                    if(p.percolates()){
+                        flag = true;
+                        break;
+                    }
+                }
+            }        
+            int opensites = p.numberOfOpenSites();
+            x[i] = (double)opensites / (n * n); 
+        }
+    }
+    
+    public double mean(){
+        return StdStats.mean(x);
+    }
+
+    public double stddev(){
+        return StdStats.stddev(x);
+    }
+    
+    public double confidenceLo(){
+        double avgx = mean();
+        double s = Math.sqrt(stddev());
+        return avgx - 1.96 * s / Math.sqrt(trials);
+    }
+    public double confidenceHi(){
+        double avgx = mean();
+        double s = Math.sqrt(stddev());
+        return avgx + 1.96 * s / Math.sqrt(trials);
+    }
+    public static void main(String[] args){
+        n = Integer.parseInt(args[0]);
+        trials = Integer.parseInt(args[1]);
+        
+        PercolationStats ps = new PercolationStats(n, trials);
+        StdOut.printf("mean                    = %f\n", ps.mean());
+        StdOut.printf("stddev                  = %f\n", ps.stddev());
+        StdOut.printf("95%% confidence interval = [%f, %f]",ps.confidenceLo(), ps.confidenceHi());
+    }
+}
